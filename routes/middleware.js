9445f930302f26c683dabeca12808e261091c07f
wrxavex <wrxavex@gmail.com>
2016-12-12 19:08:39 +0800

    change resume position

--- routes/middleware.js
+++ routes/middleware.js
@@ -20,8 +20,8 @@ var _ = require('lodash');
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'Resume', key: 'resume', href: '/resume'},
+		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'works', key: 'gallery', href: '/gallery' },
 		{ label: 'Contact', key: 'contact', href: '/contact' }
 	];
