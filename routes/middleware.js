ac3dfbb603042ce43f87b67a5d001a20a0f006fb
wrxavex <wrxavex@gmail.com>
2016-12-12 23:18:42 +0800

    add about

--- routes/middleware.js
+++ routes/middleware.js
@@ -20,6 +20,7 @@ var _ = require('lodash');
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
+		{ label: 'About', key: 'about', href: '/about'},
 		{ label: 'Resume', key: 'resume', href: '/resume'},
 		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'works', key: 'gallery', href: '/gallery' },
